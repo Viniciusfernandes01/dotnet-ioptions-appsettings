4eb98ea12ad7a1d1430ed7d75279de3ffa50ef2b
viniciusfernandes01 <vinicius.f.fernandes@hotmail.com>
2021-08-24 16:36:41 -0300

    Add project files.

---
+++ IOptionsAppSettingsDotnet/Configuration/ApiSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace IOptionsAppSettingsDotnet.Configuration
+{
+    public class ApiSettings
+    {
+        public string Url { get; set; }
+        public string QueryParameter { get; set; }
+        public string Port { get; set; }
+    }
+}
---
+++ IOptionsAppSettingsDotnet/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using IOptionsAppSettingsDotnet.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace IOptionsAppSettingsDotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly IOptions<ApiSettings> _apiSettings;
+
+        public WeatherForecastController(IOptions<ApiSettings> apiSettings)
+        {
+            _apiSettings = apiSettings;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_apiSettings);
+        }
+    }
+}
